336b6f5ad44b9c6bfdfdf2495934697a7b751385
Lane <zhanglintc623@gmail.com>
2014-05-04 23:47:00 +0800

    move, die

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,54 +1,187 @@
-ï»¿#include "snake.h"
+#include "snake.h"
 
 int main()
 {
-	return 0;
+
+    Initialize();
+    drawFrame(LEFT, TOP, RIGHT, BOTTOM, "¡õ", "¡õ");
+
+    playing();
+
+    GameOver();
+    return 0;
 }
 
-void SetPos(COORD a)// set cursor 
+void drawOne(int x, int y, char ch[])
+{
+    SetPos(x, y);
+    cout<<ch;
+}
+
+Snake::Snake()
+{
+    node[0].x=40;
+    node[0].y=12;
+    node[1].x=38;
+    node[1].y=12;
+    node[2].x=36;
+    node[2].y=12;
+    node_len=3;
+    derection=CTRL_RIGHT;
+}
+
+void Snake::move()
+{
+    clear();
+    for(int i=node_len-1; i>0; i--)
+    {
+        node[i]=node[i-1];
+    }
+    switch(derection)
+    {
+        case CTRL_UP:
+            node[0].y-=1;
+            break;
+        case CTRL_DOWN:
+            node[0].y+=1;
+            break;
+        case CTRL_LEFT:
+            node[0].x-=2;
+            break;
+        case CTRL_RIGHT:
+            node[0].x+=2;
+            break;
+    }
+    draw();
+}
+
+void Snake::judge()
+{
+    
+}
+
+void Snake::clear()
+{
+    for(int i=0;i<node_len; i++)
+    {
+        drawOne(node[i].x, node[i].y, "  ");
+    }
+}
+
+void Snake::draw()
+{
+    for(int i=0;i<node_len; i++)
+    {
+        if(i==0)
+        {
+			drawOne(node[i].x, node[i].y, "¡ñ");
+        }
+        else
+        {
+            drawOne(node[i].x, node[i].y, "¡ö");
+        }
+    }
+}
+
+void playing()
+{
+    int i=0;
+
+    Snake *snake=new Snake();
+    snake->draw();
+
+    while(true)
+    {
+        Sleep(4);
+        if(snake->node[0].x>RIGHT-2 || snake->node[0].x<LEFT+2 || snake->node[0].y>BOTTOM-1 || snake->node[0].y<TOP+1)
+        {
+            break;
+        }
+        if(_kbhit())
+        {
+            char derection=_getch();
+            if (CTRL_UP == derection || CTRL_DOWN == derection || CTRL_RIGHT == derection || CTRL_LEFT == derection)
+            {
+                snake->derection = derection;
+            }
+        }
+        i++;
+        if(i==10)
+        {
+            snake->move();
+            i=0;
+        }
+    }
+}
+
+void GameOver()
+{
+    system("cls");
+    SetPos(36,12);
+    char str[]="Game Over!";
+	cout<<str<<endl;
+	getchar();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void SetPos(COORD position)// set cursor 
 {
     HANDLE out=GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(out, a);
+    SetConsoleCursorPosition(out, position);
 }
 
-void SetPos(int i, int j)// set cursor
+void SetPos(int x, int y)// set cursor
 {
-    COORD pos={i, j};
-    SetPos(pos);
+    COORD position={x, y};
+    SetPos(position);
 }
 
-void HideCursor()
+void HideCursor() // hide cursor
 {
     CONSOLE_CURSOR_INFO cursor_info = {1, 0}; 
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
 
-//æŠŠç¬¬yè¡Œï¼Œ[x1, x2) ä¹‹é—´çš„åæ ‡å¡«å……ä¸º ch
-void drawRow(int y, int x1, int x2, char ch)
+//°ÑµÚyÐÐ£¬[x1, x2) Ö®¼äµÄ×ø±êÌî³äÎª ch
+void drawRow(int y, int x1, int x2, char ch[]) //zhanglin
 {
     SetPos(x1,y);
-    for(int i = 0; i <= (x2-x1); i++)
+    for(int i = 0; i <= (x2-x1); i+=2)
         cout<<ch;
 }
 
-//åœ¨a, b çºµåæ ‡ç›¸åŒçš„å‰æä¸‹ï¼ŒæŠŠåæ ‡ [a, b] ä¹‹é—´å¡«å……ä¸º ch
-void drawRow(COORD a, COORD b, char ch)
+//ÔÚ left, right ×Ý×ø±êÏàÍ¬µÄÇ°ÌáÏÂ£¬°Ñ×ø±ê [left, right] Ö®¼äÌî³äÎª ch
+void drawRow(COORD left, COORD right, char ch[]) //zhanglin
 {
-    if(a.Y == b.Y)
-        drawRow(a.Y, a.X, b.X, ch);
+    if(left.Y == right.Y)
+        drawRow(left.Y, left.X, right.X, ch);
     else
     {
         SetPos(0, 25);
-        cout<<"error code 01ï¼šæ— æ³•å¡«å……è¡Œï¼Œå› ä¸ºä¸¤ä¸ªåæ ‡çš„çºµåæ ‡(x)ä¸ç›¸ç­‰";
+        cout<<"error code 01£ºÎÞ·¨Ìî³äÐÐ£¬ÒòÎªÁ½¸ö×ø±êµÄ×Ý×ø±ê(x)²»ÏàµÈ";
         system("pause");
     }
 }
 
-//æŠŠç¬¬xåˆ—ï¼Œ[y1, y2] ä¹‹é—´çš„åæ ‡å¡«å……ä¸º ch
-void drawCol(int x, int y1, int y2, char ch)
+//°ÑµÚxÁÐ£¬[y1, y2] Ö®¼äµÄ×ø±êÌî³äÎª ch
+void drawCol(int x, int y1, int y2, char ch[]) //zhanglin
 {
     int y=y1;
-    while(y!=y2+1)
+    while(y!=y2)
     {
         SetPos(x, y);
         cout<<ch;
@@ -56,53 +189,69 @@ void drawCol(int x, int y1, int y2, char ch)
     }
 }
 
-//åœ¨a, b æ¨ªåæ ‡ç›¸åŒçš„å‰æä¸‹ï¼ŒæŠŠåæ ‡ [a, b] ä¹‹é—´å¡«å……ä¸º ch
-void drawCol(COORD a, COORD b, char ch)
+//ÔÚ left, right ºá×ø±êÏàÍ¬µÄÇ°ÌáÏÂ£¬°Ñ×ø±ê [left, right] Ö®¼äÌî³äÎª ch
+void drawCol(COORD left, COORD right, char ch[]) //zhanglin
 {
-    if(a.X == b.X)
-        drawCol(a.X, a.Y, b.Y, ch);
+    if(left.X == right.X)
+        drawCol(left.X, left.Y, right.Y, ch);
     else
     {
         SetPos(0, 25);
-        cout<<"error code 02ï¼šæ— æ³•å¡«å……åˆ—ï¼Œå› ä¸ºä¸¤ä¸ªåæ ‡çš„æ¨ªåæ ‡(y)ä¸ç›¸ç­‰";
+        cout<<"error code 02£ºÎÞ·¨Ìî³äÁÐ£¬ÒòÎªÁ½¸ö×ø±êµÄºá×ø±ê(y)²»ÏàµÈ";
         system("pause");
     }
 }
 
-//å·¦ä¸Šè§’åæ ‡ã€å³ä¸‹è§’åæ ‡ã€ç”¨rowå¡«å……è¡Œã€ç”¨colå¡«å……åˆ—
-void drawFrame(COORD a, COORD  b, char row, char col)
+//×óÉÏ½Ç×ø±ê¡¢ÓÒÏÂ½Ç×ø±ê¡¢ÓÃrowÌî³äÐÐ¡¢ÓÃcolÌî³äÁÐ
+void drawFrame(COORD leftup, COORD  rightdown, char row[], char col[])
 {
-    drawRow(a.Y, a.X+1, b.X-1, row);
-    drawRow(b.Y, a.X+1, b.X-1, row);
-    drawCol(a.X, a.Y+1, b.Y-1, col);
-    drawCol(b.X, a.Y+1, b.Y-1, col);
+    drawRow(   leftup.Y, leftup.X, rightdown.X, row);       //³õÊ¼ÐÐ
+    drawRow(rightdown.Y, leftup.X, rightdown.X, row);       //×îºóÐÐ
+    drawCol(   leftup.X, leftup.Y, rightdown.Y, col);       //³õÊ¼ÁÐ
+    drawCol(rightdown.X, leftup.Y, rightdown.Y, col);       //×îºóÁÐ
 }
 
-void drawFrame(int x1, int y1, int x2, int y2, char row, char col)
+void drawFrame(int x1, int y1, int x2, int y2, char row[], char col[])
 {
-    COORD a={x1, y1};
-    COORD b={x2, y2};
-    drawFrame(a, b, row, col);
+    COORD leftup={x1, y1};
+    COORD rightdown={x2, y2};
+    drawFrame(leftup, rightdown, row, col);
 }
 
-void drawFrame(Frame frame, char row, char col)
+void drawFrame(Frame frame, char row[], char col[])
 {
-    COORD a = frame.position[0];
-    COORD b = frame.position[1];
-    drawFrame(a, b, row, col);
+    COORD leftup = frame.position[0];
+    COORD rightdown = frame.position[1];
+    drawFrame(leftup, rightdown, row, col);
 }
 
-int random(int a, int b)
+int random(int min, int max)
 {
-    int c=(rand() % (a-b))+ a;
+    if(max<min)swap(&min,&max);
+    int c=(rand() % (min-max))+ min;
     return c;
 }
 
-//åœ¨ä¸¤ä¸ªåæ ‡åŒ…æ‹¬çš„çŸ©å½¢æ¡†å†…éšæœºäº§ç”Ÿä¸€ä¸ªåæ ‡
+//ÔÚÁ½¸ö×ø±ê°üÀ¨µÄ¾ØÐÎ¿òÄÚËæ»ú²úÉúÒ»¸ö×ø±ê
 COORD random(COORD leftup, COORD rightdown)
 {
     int x=random(leftup.X, rightdown.X);
     int y=random(leftup.Y, rightdown.Y);
     COORD c={x, y};
     return c;
+}
+
+void Initialize()
+{
+    _tsetlocale(LC_ALL, L"CHS");        //set locale codepage (Chinese)
+    srand((unsigned)time(NULL));        //Random number seed initial, 2014.03.16 zhanglin
+    HideCursor();
+}
+
+void swap(int *a, int *b)
+{
+    int m;
+    m = *a;
+    *a = *b;
+    *b = m;
 }
--- src/snake.h
+++ src/snake.h
@@ -1,10 +1,18 @@
-﻿#include <iostream>
+﻿#ifndef _SNAKE_H_
+#define _SNAKE_H_
+
+//inlcude
+#include <stdio.h>
+#include <iostream>
+#include <tchar.h>
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
-#include <string>
+#include <string.h>
+#include <locale.h>
 using namespace std;
 
+//define
 #define NODE_MAX		200
 #define CTRL_UP			72
 #define CTRL_DOWN		80
@@ -12,31 +20,70 @@ using namespace std;
 #define CTRL_LEFT		75
 #define CTRL_SPACE		32
 
-class snake
-{
-public:
-	snake();
-	void move();
-	void move(int);
-	void draw();
-private:
-	int x[NODE_MAX];
-	int y[NODE_MAX];
-	int derection;
-	int node;
-	int live;
-};
+#define TOP				0
+#define BOTTOM			24
+#define LEFT			0
+#define RIGHT			78
 
-class food
-{
-public:
-	int a;
-private:
-	int b;
-};
-
+//typedef
 typedef struct Frame
 {
 	COORD position[2];
 	int flag;
 }Frame;
+
+typedef struct Node
+{
+	int x;
+	int y;
+}Node;
+
+//class
+class Snake
+{
+public:
+	int derection;
+	Node node[NODE_MAX];
+	Snake();
+	void move();
+	void move(int);
+	void judge();
+	void eat();
+	void draw();
+private:
+	
+	int node_len;
+	int live;
+	void clear();
+	//void draw();
+};
+
+class food
+{
+public:
+	food();
+private:
+	int x;
+	int y;
+};
+
+//declare
+void SetPos(COORD a);
+void SetPos(int i, int j);
+void HideCursor();
+void drawRow(int y, int x1, int x2, char ch[]);
+void drawRow(COORD a, COORD b, char ch[]);
+void drawCol(int x, int y1, int y2, char ch[]);
+void drawCol(COORD a, COORD b, char ch[]);
+void drawFrame(COORD a, COORD  b, char row[], char col[]);
+void drawFrame(int x1, int y1, int x2, int y2, char row[], char col[]);
+void drawFrame(Frame frame, char row[], char col[]);
+int random(int a, int b);
+COORD random(COORD leftup, COORD rightdown);
+void Initialize();
+void swap(int *a, int *b);
+void drawOne(int x, int y, char ch[]);
+void playing();
+void GameOver();
+
+#endif //_SNAKE_H_
