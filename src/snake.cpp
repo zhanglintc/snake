3b2836523aa98fd1dbe215b5d346c53acea8fa07
Lane <zhanglintc623@gmail.com>
2014-05-30 11:05:56 +0800

    comment

--- src/snake.cpp
+++ src/snake.cpp
@@ -6,13 +6,14 @@ Main process here.  http://zhanglintc.co
 
 #include "snake.h"
 
-//these are assigned in Initialize()
+//these variables are assigned in funtction Initialize()
 uchar const *g_const_circ_b;    //global ●
 uchar const *g_const_rect_b;    //global ■
 uchar const *g_const_rect_w;    //global □
 uchar const *g_const_star_b;    //global ★
 uchar const *g_const_star_w;    //global ※
 
+//global variable declares
 char g_Local_Language[10];      //global language info
 int  g_difficulty=0;            //global difficulty
 int  g_score=0;                 //global score
@@ -25,7 +26,7 @@ int  Food::x=0;
 int  Food::y=0;
 
 /*******************************************************
-Function: entrance
+Function: main entrance
 Argument: none
 Return  : int
 *******************************************************/
@@ -72,12 +73,12 @@ void Playing()
             break;
     }
 
-    Snake *snake = new Snake(); // new one snake
+    Snake *snake = new Snake();     // new one snake
     Food  *food = new Food(snake);  //new a food
 
-    while(snake->getlife()) //while alive
+    while(snake->getlife()) //while alive, process the sanke move, snake eat things. inportant block
     {
-        Sleep(SLEEP_TIME); //game speed
+        Sleep(SLEEP_TIME);  //game speed
         counter++;
 
         //debug print
@@ -85,7 +86,7 @@ void Playing()
         //cout<<counter;
         if(_kbhit())
         {
-            gotten=_getch(); //get direction(and maybe not direction input)
+            gotten=_getch();        //get direction(but can be not direction input)
 
             if(gotten==-32)continue;
             if(gotten==CTRL_SPACE || gotten==CTRL_ENTER)Pause();
@@ -94,48 +95,48 @@ void Playing()
             //when cache1st contains data(which means direction) && gotten is not in the same line with cache1st
             if(isDirection(cache1st)==true && isSameLine(gotten,cache1st)==false)
             {
-                cache2nd=gotten; //store gotten in cache2nd
+                cache2nd=gotten;    //store gotten in cache2nd
             }
 
             //when gotten is direction && gotten is not in the same line with snake's direction 
             if(isDirection(gotten)==true && isSameLine(gotten,snake->getdirection())==false)
             {
-                cache1st=gotten; //store gotten in cache1st
+                cache1st=gotten;    //store gotten in cache1st
             }
         }
-        if(counter>=GameSpeed) //each GameSpeed times, judge move
+        if(counter>=GameSpeed)      //each GameSpeed times, judge move
         {
-            counter=1; //reset counter
+            counter=1;  //reset counter
 
             //cache2nd -> cache1st -> direction -> snake->move(direction)
-            direction=cache1st; //assembly line work
-            cache1st=cache2nd;  //assembly line work
+            direction=cache1st;     //assembly line work
+            cache1st=cache2nd;      //assembly line work
 
             snake->move(direction); //snake wriggling
 
             //eat food
             if(snake->node[0].x==food->x && snake->node[0].y==food->y)
             {
-                snake->eat(food); //^_^
+                snake->eat(food);       //^_^
 				food = new Food(snake); //after eat, generate a new food
 
-                g_score+=g_difficulty;
+                g_score+=g_difficulty;  //difficulty means point
                 g_eaten+=1;
                 PrintInfo(INFO_UPDT, PLAYING);
             }
 
-            //hit walls, die
+            //hit walls, die (to be or not to be...?)
             if(snake->node[0].x>RIGHT-2 || snake->node[0].x<LEFT+2 || snake->node[0].y>BOTTOM-1 || snake->node[0].y<TOP+1)
             {
-                snake->setlife(false); //T_T
+                snake->setlife(false);      //T_T
             }
 
-            //hit itself, die
+            //hit itself, die (to be or not to be...?)
             for(int i=3;i<snake->getsnakelength();i++)
             {
                 if(snake->node[0].x==snake->node[i].x && snake->node[0].y==snake->node[i].y)
                 {
-                    snake->setlife(false); //T_T
+                    snake->setlife(false);  //T_T
                 }
             }
         }
@@ -191,7 +192,7 @@ Snake::Snake()
     node[2].x = (RIGHT-LEFT)/2-4;
     node[2].y = (BOTTOM-TOP)/2;
 	
-	if(node[0].x%2!=0) //set snake in even position
+	if(node[0].x%2!=0)      //set snake in even position
 	{
 		for(int i=0;i<length;i++)
 		{
@@ -199,7 +200,7 @@ Snake::Snake()
 		}
 	}
 
-    draw(); //show this snake
+    draw();                 //show this snake
 }
 
 /*******************************************************
@@ -222,20 +223,20 @@ void Snake::move(int direction)
     //else this->direction stay the same
 
     //wriggling
-    for(int i=length-1; i>0; i--) //from last body to first body(not contain head)
+    for(int i=length-1; i>0; i--)   //from last body to first body(not contain head)
     {
-        if(i==length-1) //if this is the last node
+        if(i==length-1)             //if this is the last node
         {
             if(node[i].x==node[i-1].x && node[i].y==node[i-1].y)
             {
-                continue; //if last and last-1 is the same, do not clean
+                continue;           //if last and last-1 is the same, do not clean
             }
             drawOne(node[i].x, node[i].y, (char *)ICON_NULL); //clean last node
         }
-        node[i]=node[i-1]; //wriggle forward
+        node[i]=node[i-1];          //wriggle forward
     }
 
-    switch(this->direction) //head move
+    switch(this->direction)         //head move
     {
         case CTRL_UP:
             node[0].y-=1;
@@ -304,15 +305,15 @@ Food::Food(Snake *snake)
 {
     /* get food(x, y) */
     x=random(LEFT+2,RIGHT-2); 
-    while(x%2!=0)x=random(LEFT+2,RIGHT-2); //adjust x to even number
+    while(x%2!=0)x=random(LEFT+2,RIGHT-2);              //adjust x to even number
     y=random(TOP+1,BOTTOM-1);
 
-    for(int i=0;i<snake->getsnakelength();i++) //for every node of this snake
+    for(int i=0;i<snake->getsnakelength();i++)          //for every node of this snake
     {
-        if(x==snake->node[i].x && y==snake->node[i].y) //if this food is in one node of this snake
+        if(x==snake->node[i].x && y==snake->node[i].y)  //if this food is in one node of this snake
         {
-			Food::Food(snake); //new another food
-			return; //return to avoid showing it
+			Food::Food(snake);                          //new another food
+			return;                                     //return to avoid showing it
         }
     }
     drawOne(x,y,(char *)g_const_star_b); //show this food
